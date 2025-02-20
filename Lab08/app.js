e74f3106d84cade4a867bed49f0e055735598a62
Mauricio Olguin <mauolguisanchez@gmail.com>
2025-02-20 12:49:47 -0600

    Testing Node: Async things

--- Lab08/app.js
+++ Lab08/app.js
@@ -1,4 +1,15 @@
+// Notas clase: Aprendiendo Node.js
+
 const fileSystem = require('fs');
 
-fileSystem.writeFileSync('hola.txt','');
+fileSystem.writeFileSync('hola.txt','Hola desde node');
+
+// Ejemplo de cosas asíncronas:
+const arreglo = [5000,60,90,100,10,20,1000,0,120,2000,340,50]
+
+for(let item of arreglo){
+    setTimeout(() => {
+        console.log(item)
+    }, item);
+}
 
