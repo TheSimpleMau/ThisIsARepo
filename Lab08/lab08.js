2bf8ee70236f1d32a341c6d56607f4a0252bdc2f
Mauricio Olguin <mauolguisanchez@gmail.com>
2025-02-20 15:43:49 -0600

    Lab08: Donde exercise 2

--- Lab08/lab08.js
+++ Lab08/lab08.js
@@ -12,3 +12,12 @@ function arrayMean(array){
 let testArray = [1,2,3,4];
 console.log("Esto es una prueba de la función array: " + arrayMean(test));
 
+
+// Ejercicio 2
+function writeStringOnFile(string){
+    const fileSystem = require('fs');
+    fileSystem.writeFileSync('StringInput.txt',string);
+    console.log("File with string input done!");
+}
+
+// Ejercicio 3
