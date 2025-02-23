25b19a20f6b2aeff9ebe1819a845e3d295eb3a3b
Mauricio Olguin <mauolguisanchez@gmail.com>
2025-02-22 20:29:26 -0600

    Lab06: Remaking password checker

--- Lab06/index.js
+++ Lab06/index.js
@@ -1,12 +1,19 @@
+// Caracteres para verificar
+const specialCharacters = /[!"#$%&/()\=?¡+*{}\[\];:,.|°]/;
+// const lowerCharacters = /qwertyuiopasdfghjklñzxcvbnm/;
+const upperCharacters = /[A-Z]/;
+const numericCharacters = /\d/;
 
 // Obteniendo los elementos
 const passwordTry = document.getElementById("passwTry");
 const passwordChecker = document.getElementById("passwChecker");
 const passwordStatus = document.getElementById("passwStatus");
+const passwordStrong = document.getElementById("passwStrong");
 
 //Bandera para saber si es que hay algo escrito dentro de passwTry
 let passwordSet = false;
 
+// Para saber si ya escribió algo dentro del primer campo
 passwordTry.addEventListener("input", function () {
     if (this.value.length > 0) {
         passwordSet = true; // Marcar que ya se escribió algo
@@ -15,6 +22,32 @@ passwordTry.addEventListener("input", function () {
         passwordStatus.innerHTML = '<h3 class="title is-3">Escriba algo dentro del campo de contraseña</h3>';
     }
 });
+
+passwordTry.addEventListener("input", function() {
+    passwordStrong.innerHTML = '';
+    let text = '<p>'
+    if (upperCharacters.test(this.value)) {
+        text += '✅ Mayúscula <br>';
+    } else {
+        text += '❌ Mayúscula <br>'
+    }
+    if (specialCharacters.test(this.value)) {
+        text += '✅ Caracter especial <br>';
+    } else {
+        text += '❌ Caracter especial <br>'
+    }
+    if (numericCharacters.test(this.value)) {
+        text += '✅ Número <br>';
+    } else {
+        text += '❌ Número <br>';
+    }
+    if (this.value.length > 8) {
+        text += '✅ Mayor a 8 caracteres';
+    } else {
+        text += '❌ Mayor a 8 caracteres';
+    }
+    passwordStrong.innerHTML = text + '</p>'
+})
 
 // Para validar las contraseñas
 passwordChecker.addEventListener("input", function () {
--- Lab08/lab08.js
+++ Lab08/lab08.js
@@ -10,14 +10,16 @@ function arrayMean(array){
 }
 
 let testArray = [1,2,3,4];
-console.log("Esto es una prueba de la función array: " + arrayMean(test));
+console.log("Esto es una prueba de la función array: " + arrayMean(testArray));
 
 
 // Ejercicio 2
 function writeStringOnFile(string){
     const fileSystem = require('fs');
     fileSystem.writeFileSync('StringInput.txt',string);
-    console.log("File with string input done!");
+    console.log("Archivo con cadena de texto añadido a archivo StringInput.txt");
 }
+
+writeStringOnFile("Hola desde función");
 
 // Ejercicio 3
