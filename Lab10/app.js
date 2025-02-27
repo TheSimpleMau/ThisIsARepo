720a485fd87028e0a75e1bb69132068236f9e285
Mauricio Olguin <mauolguisanchez@gmail.com>
2025-02-26 22:45:49 -0600

    Lab10: Done

--- Lab10/app.js
+++ Lab10/app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const path = require('path');
 
 function getHTML(ruta){
     html = fs.readFileSync(ruta,{encoding: 'utf-8', flag: 'r'});
@@ -7,32 +8,28 @@ function getHTML(ruta){
 };
 
 const server = http.createServer((request, response) => {
+
     let html = '';
-    console.log(request.url);
-    const datos = [];
-    request.on('data',(data)=>{
-        datos.push(data);
-    })
-    console.log(Buffer.concat(datos).toString());
+    
     if ((request.url == "/index.html" || request.url == "/") && request.method == "GET"){
         response.setHeader('Content-Type', 'text/html');
         html = getHTML('./index.html');
         response.write(html);
         response.end();
     }
-    else if (request.url == '/laboratorios/Lab03' && request.method == "POST"){
+    else if (request.url == '/laboratorios/Lab03' && (request.method == "POST" || request.method == "GET")){
         response.setHeader('Content-Type', 'text/html');
         html = getHTML('./laboratorios/Lab03/Lab03.html');
         response.write(html);
         response.end();
     }
-    else if (request.url == '/laboratorios/Lab05' && request.method == "POST"){
+    else if (request.url == '/laboratorios/Lab05' && (request.method == "POST" || request.method == "GET")){
         response.setHeader('Content-Type', 'text/html');
         html = getHTML('./laboratorios/Lab05/index.html');
         response.write(html);
         response.end();
     }
-    else if (request.url == '/laboratorios/Lab06' && request.method == "POST"){
+    else if (request.url == '/laboratorios/Lab06' && (request.method == "POST" || request.method == "GET")){
         response.setHeader('Content-Type', 'text/html');
         html = getHTML('./laboratorios/Lab06/index.html');
         response.write(html);
--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # This Is A Repo!!!
 
-## Notas sobre el repositorio
-
-- A partir del laboratorio 10, todos lo laboratorio serán sobre el mismo archivo index.{html, css, js} etc. 
