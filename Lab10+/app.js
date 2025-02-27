5dba7eacacba55a4bd6cf04cba3e939ae5994c50
Mauricio Olguin <mauolguisanchez@gmail.com>
2025-02-26 22:22:10 -0600

    Lab10: Done loading htmls on labs, resolving content (js,css,imgs) problems.

--- Lab10+/app.js
+++ Lab10+/app.js
@@ -8,14 +8,38 @@ function getHTML(ruta){
 
 const server = http.createServer((request, response) => {
     let html = '';
+    console.log(request.url);
+    const datos = [];
+    request.on('data',(data)=>{
+        datos.push(data);
+    })
+    console.log(Buffer.concat(datos).toString());
     if ((request.url == "/index.html" || request.url == "/") && request.method == "GET"){
         response.setHeader('Content-Type', 'text/html');
         html = getHTML('./index.html');
         response.write(html);
         response.end();
     }
-    if ((request.url == '/laboratorios'))
+    else if (request.url == '/laboratorios/Lab03' && request.method == "POST"){
+        response.setHeader('Content-Type', 'text/html');
+        html = getHTML('./laboratorios/Lab03/Lab03.html');
+        response.write(html);
+        response.end();
+    }
+    else if (request.url == '/laboratorios/Lab05' && request.method == "POST"){
+        response.setHeader('Content-Type', 'text/html');
+        html = getHTML('./laboratorios/Lab05/index.html');
+        response.write(html);
+        response.end();
+    }
+    else if (request.url == '/laboratorios/Lab06' && request.method == "POST"){
+        response.setHeader('Content-Type', 'text/html');
+        html = getHTML('./laboratorios/Lab06/index.html');
+        response.write(html);
+        response.end();
+    }
     else {
+        response.statusCode = 404;
         response.setHeader('Content-Type', 'text/html');
         html = getHTML('./NotFound.html');
         response.write(html);
